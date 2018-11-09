58a7e18e767389674e468ec2efbfcd4b1e11abca
Zhang Zengbo <zengbo.zhang@gmail.com>
2018-11-09 18:13:32 +0800

    ui change

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+    QFont font("PT Sans");
+    font.setStyleHint(QFont::SansSerif);
+    font.setPointSize(15);
+    font.setPixelSize(15);
+    
+
     QApplication a(argc, argv);
+    QApplication::setFont(font);
     MainWindow w;
     w.show();
 
