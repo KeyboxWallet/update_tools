4fd8a4f3d6f824c08e46fd93c803b65fc0765b76
Zhang Zengbo <zengbo.zhang@gmail.com>
2018-11-09 21:54:14 +0800

    fix an issue for mac

--- macos_release.sh
+++ macos_release.sh
@@ -1,6 +1,7 @@
 git clean . -xdf
 cmake -DCMAKE_BUILD_TYPE=Release . &&
 make &&
+macdeployqt update_tools.app &&
 codesign --deep --force --verbose --sign 'Developer ID App' update_tools.app/ &&
 appdmg dmg_spec.json update_tools.dmg 
 
